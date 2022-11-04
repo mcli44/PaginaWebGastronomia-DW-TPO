d0bc29a69a42e678747909aeca20ebae3508e2aa
mcli44 <57851097+mcli44@users.noreply.github.com>
2022-11-04 14:58:14 -0300

    Beta v4.0.0

--- ajuste.js
+++ ajuste.js
@@ -1,41 +1,52 @@
 function creadorNAV(){
-    const borrar = document.getElementById("desplegable");
+    const borrar = document.getElementById("botonnav");
     borrar.style.display = "none";
     
     
 
     let menu = document.createElement("NAV");
-    menu.id = "navbar"
-    let produc = document.createElement("a");
-    produc.innerHTML = "NUESTRAS ESPECIALIDADES";
-    produc.href = "menu.html";
+    menu.id = "navbar";
 
-    let direc = document.createElement("a");
-    direc.innerHTML = "DONDE ENCONTRARNOS";
-    direc.href = "direcion.html";
+    /*var imgIni = document.createElement("img");
+    imgIni.classList.add("imgIni")
+    imgIni.setAttribute("src", "Imagenes/hogar.png");
+    imgIni.setAttribute("height", "20");
+    imgIni.setAttribute("width", "20");
+    imgIni.setAttribute("alt", "Imghome");*/
+    let inicio = document.createElement("a");
+    inicio.innerHTML = "Inicio";
+    inicio.href = "index.html";
+    /*inicio.prepend(imgIni);*/
+    
+    /*var imgSer = document.createElement("img");
+    imgSer.classList.add("imgIni2")
+    imgSer.setAttribute("src", "Imagenes/wedding-arch-negra.png");
+    imgSer.setAttribute("height", "20");
+    imgSer.setAttribute("width", "20");
+    imgSer.setAttribute("alt", "ImgServ");*/
+    let servicios = document.createElement("a");
+    servicios.innerHTML = "Servicios";
+    servicios.href = "servicios.html";
+    /*servicios.prepend(imgSer);*/
 
-    let presu = document.createElement("a");
-    presu.innerHTML = "PRESUPUESTO";
-    presu.href = "presupuesto.html";
+    let galeria = document.createElement("a");
+    galeria.innerHTML = "Galeria";
+    galeria.href = "galeria.html";
 
-    let testi = document.createElement("a");
-    testi.innerHTML = "TESTIMONIOS";
-    testi.href = "testimonio.html";
+    let contacto = document.createElement("a");
+    contacto.innerHTML = "Contactos";
+    contacto.href = "contacto.html";
 
-    menu.appendChild(produc);
-    menu.appendChild(direc);
-    menu.appendChild(testi);
-    menu.appendChild(presu);
+    menu.appendChild(inicio);
+    menu.appendChild(servicios);
+    menu.appendChild(galeria);
+    menu.appendChild(contacto);
     document.getElementById("cabecera").appendChild(menu);
 }
 
-function borrarBotones(){
-    const borrar1 = document.getElementById("botonesIndex");
-    borrar1.style.display = "none";
-}
 
 function reactivarMenu(){
-    const des = document.getElementById("desplegable");
+    const des = document.getElementById("botonnav");
     des.style.display = "none";
     const menu = document.getElementById("navbar");
     menu.style.display = "flex";
@@ -43,37 +54,11 @@ function reactivarMenu(){
 }
 
 function reactivarDes(){
-    const des = document.getElementById("desplegable");
+    const des = document.getElementById("botonnav");
     des.style.display = "inline-block"; 
     
     const menu = document.getElementById("navbar");
     menu.style.display = "none";
 }
 
-function reactivarBotones(){
-    const borrar1 = document.getElementById("botonesIndex");
-    borrar1.style.display = "flex";
-}
 
-function verificacionForm(form){
-
-    let nomb = form.nombre.value;
-    String(nomb);
-
-    let textoAux = form.texto.value;
-    String(textoAux);
-    
-    if(/^[a-zA-Z]+$/.test(nomb) == true){
-        if(textoAux != ""){
-            alert("Presupuesto enviado. Muchas gracias! :)");
-        }
-        
-        
-    }
-    else{
-        alert("Ingresa un nombre correcto");
-        event.preventDefault();
-    }
-   
-    
-}
